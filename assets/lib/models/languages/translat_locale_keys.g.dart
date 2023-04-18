0987610fd26e1e3635ce097fdcc140545ce5f1f5
trinetron <tryliry@gmail.com>
2023-04-18 18:43:53 +0300

    v1

---
+++ assets/lib/models/languages/translat_locale_keys.g.dart
@@ -0,0 +1,15 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const c_dload = 'c_dload';
+  static const c_sec_pol = 'c_sec_pol';
+  static const c_man = 'c_man';
+  static const c_txt1 = 'c_txt1';
+  static const c_txt2 = 'c_txt2';
+  static const c_push_me = 'c_push_me';
+  static const c_txt3 = 'c_txt3';
+  static const c_txt4 = 'c_txt4';
+  static const c_txt5 = 'c_txt5';
+  static const c_txt6 = 'c_txt6';
+
+}
